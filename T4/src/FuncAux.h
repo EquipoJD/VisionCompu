54ec4e6139226927023aaa794a99dca05180d317
TheRabid <jai_mi_to@hotmail.es>
2015-04-13 23:15:31 +0200

    T4 preparado para empezar a trabajar

--- T4/src/FuncAux.h
+++ T4/src/FuncAux.h
@@ -8,10 +8,5 @@
 #ifndef FUNCAUX_H_
 #define FUNCAUX_H_
 
-bool isHorizontal(float radian, float desviacion);
-bool isVertical(float radian, float desviacion);
-void mostrarImagen(std::string tituloFoto, cv::Mat imagen, bool espera);
-cv::Mat calcularPuntoFuga(cv::Mat image, cv::Mat grad, cv::Mat orientacion, bool correccion,
-		int anchuraFranja, bool verVotantes);
 
 #endif /* FUNCAUX_H_ */
--- T4/src/T4.cpp
+++ T4/src/T4.cpp
@@ -1,18 +1,31 @@
 /*
- * FuncAux.cpp
+ * T3.cpp
  *
  *  Created on: 08/03/2015
  *      Author: Jaime. David.
  */
 
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <string>
 #include "FuncAux.h"
 
 using namespace std;
 using namespace cv;
 
 /* Variables globales */
+
+/*
+ * Función main del Trabajo 4 de Visión por Computador
+ * Gestiona el funcionamiento principal del programa
+ */
+
+int main() {
+	return 0;
+}
+
